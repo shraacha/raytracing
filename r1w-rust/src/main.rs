38dc9fa46864aa1d36eaae7d604a383d769776b1
Anish R <racharlaanish@gmail.com>
2023-07-29 22:13:52 -0400

    rustpilled

---
+++ r1w-rust/src/main.rs
@@ -0,0 +1,13 @@
+use std::io::prelude::*;
+
+fn main() {
+    // Bufwriter
+    let out = std::io::BufWriter::new(std::io::stdout().lock());
+
+    // Image
+    let image_width:  i32 = 256;
+    let image_height: i32 = 256;
+
+    // Render
+
+}
---
+++ r1w-rust/target/debug/deps/r1w_rust-9b1d12c8000b711e.d
@@ -0,0 +1,9 @@
+/Users/anish/Documents/Github/raytracing/r1w-rust/target/debug/deps/r1w_rust-9b1d12c8000b711e.rmeta: src/main.rs Cargo.toml
+
+/Users/anish/Documents/Github/raytracing/r1w-rust/target/debug/deps/r1w_rust-9b1d12c8000b711e.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
