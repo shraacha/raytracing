583dd4c86828b29cbb07cafc2044c2d1f31b5bb8
shracha <racharlaanish@gmail.com>
2023-09-08 17:41:43 -0400

    progress

--- r1w-rust/src/main.rs
+++ r1w-rust/src/main.rs
@@ -3,6 +3,7 @@ use std::io::prelude::*;
 fn main() {
     // Bufwriter
     let mut out = std::io::BufWriter::new(std::io::stdout().lock());
+    let mut err = std::io::BufWriter::new(std::io::stderr().lock());
 
     // Image
     let image_width:  i32 = 256;
@@ -14,11 +15,12 @@ fn main() {
     writeln!(out, "{image_width} {image_height}").unwrap();
     writeln!(out, "255").unwrap();
 
-    for j in (0..image_height).rev() {
+    for j in 0..image_height {
+        write!(err, "\rScanlines remaining: {prog} ", prog = image_height - j).unwrap();
         for i in 0..image_width {
             let r = i as f64 / (image_width - 1) as f64;
             let g = j as f64 / (image_height - 1) as f64;
-            let b = 0.25;
+            let b = 0 as f64;
 
             let ir: i32 = (255.999 * r) as i32;
             let ig: i32 = (255.999 * g) as i32;
@@ -27,4 +29,6 @@ fn main() {
             writeln!(out, "{ir} {ig} {ib}").unwrap();
         }
     }
+
+    writeln!(err, "\rDone.                          ").unwrap();
 }
